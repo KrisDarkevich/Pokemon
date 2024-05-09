5c948c2b597c6fa833a26cd1848abb2d0b1c8909
Kseniya Boldyreva <KristinaDarkevich@mail.ru>
2024-05-09 14:40:02 +0300

    search update!

--- lib/logic/bloc.dart
+++ lib/logic/bloc.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pokemons/logic/api/pokemon_api.dart';
 import 'package:pokemons/logic/api/repository/repository.dart';
 
 abstract class ApiState {}
@@ -23,16 +24,56 @@ class GetUrlEvent extends ApiEvent {
   GetUrlEvent(this.offset);
 }
 
+class SearchEvent extends ApiEvent {
+  final String query;
+
+  SearchEvent(this.query);
+}
+
 class ApiBloc extends Bloc<ApiEvent, ApiState> {
   final PokemonRepository pokemonRepository;
+  final List<Results> _results = [];
 
   ApiBloc(this.pokemonRepository) : super(InitialState()) {
     on<GetUrlEvent>(
       (event, emit) async {
         emit(LoadingState());
+        _results.clear();
         final result = await pokemonRepository.getInfo(event.offset);
+        _results.addAll(result.pokemonApi.results);
 
         emit(SuccessState(result));
+      },
+    );
+    on<SearchEvent>(
+      (event, emit) async {
+        emit(LoadingState());
+        if (event.query.isEmpty) {
+          emit(
+            SuccessState(
+              FullInfo(
+                OnePokemon(0),
+                PokemonApi(_results.length, _results),
+              ),
+            ),
+          );
+        } else {
+          final List<Results> satisfyingPokemons = [];
+          for (var res in _results) {
+            if (res.name.contains(event.query)) {
+              satisfyingPokemons.add(res);
+            }
+          }
+
+          emit(
+            SuccessState(
+              FullInfo(
+                OnePokemon(0),
+                PokemonApi(satisfyingPokemons.length, satisfyingPokemons),
+              ),
+            ),
+          );
+        }
       },
     );
   }
--- lib/widgets/start_screen.dart
+++ lib/widgets/start_screen.dart
@@ -42,17 +42,73 @@ class _StartScreenState extends State<StartScreen> {
                   borderRadius: BorderRadius.circular(30),
                 ),
               ),
+              onFieldSubmitted: (query) {
+                context.read<ApiBloc>().add(
+                      SearchEvent(query),
+                    );
+              },
             ),
-            Expanded(
-              child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisExtent: 150,
-                ),
-                itemCount: 20,
-                itemBuilder: (context, index) => _Card(index),
-              ),
-            ),
+            BlocBuilder<ApiBloc, ApiState>(builder: (context, state) {
+              if (state is SuccessState) {
+                return Expanded(
+                  child: GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        mainAxisExtent: 150,
+                      ),
+                      itemCount: state.fullInfo.pokemonApi.results.length,
+                      itemBuilder: (context, index) {
+                        final pokemonInfo = (state.fullInfo);
+                        final urlImageString =
+                            pokemonInfo.pokemonApi.results[index].url;
+                        final Uri url = Uri.parse(urlImageString);
+                        final imageId = url.pathSegments[3];
+                        return Container(
+                          margin: const EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 12,
+                          ),
+                          decoration: BoxDecoration(
+                            color: PokeColor.white,
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: [
+                              BoxShadow(
+                                color: PokeColor.greyBlue.withOpacity(0.5),
+                                spreadRadius: 2,
+                                blurRadius: 6,
+                                offset: const Offset(5, 4),
+                              ),
+                            ],
+                          ),
+                          child: Center(
+                            child: Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Flexible(
+                                    child: Text(
+                                      pokemonInfo
+                                          .pokemonApi.results[index].name,
+                                      style: PokeStyle.name,
+                                    ),
+                                  ),
+                                  Image.network(
+                                    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$imageId.png',
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      }),
+                );
+              } else {
+                return const CircularProgressIndicator(
+                  color: PokeColor.darkRed,
+                );
+              }
+            }),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -61,9 +117,9 @@ class _StartScreenState extends State<StartScreen> {
                   onPressed: () {
                     if (index >= 20) {
                       index -= 20;
-                      return (context.read<ApiBloc>().add(
+                      return context.read<ApiBloc>().add(
                             GetUrlEvent(index),
-                          ));
+                          );
                     }
                   },
                   child: const Text('Previous'),
@@ -86,81 +142,6 @@ class _StartScreenState extends State<StartScreen> {
           ],
         ),
       ),
-    );
-  }
-}
-
-class _Card extends StatelessWidget {
-  final int index;
-  const _Card(this.index);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {},
-      child: Container(
-        margin: const EdgeInsets.symmetric(
-          vertical: 10,
-          horizontal: 12,
-        ),
-        decoration: BoxDecoration(
-          color: PokeColor.white,
-          borderRadius: BorderRadius.circular(20),
-          boxShadow: [
-            BoxShadow(
-              color: PokeColor.greyBlue.withOpacity(0.5),
-              spreadRadius: 2,
-              blurRadius: 6,
-              offset: const Offset(5, 4),
-            ),
-          ],
-        ),
-        child: Center(
-          child: _BlocColumn(
-            index: index,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class _BlocColumn extends StatelessWidget {
-  final int index;
-  const _BlocColumn({required this.index});
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<ApiBloc, ApiState>(
-      builder: (context, state) {
-        if (state is SuccessState) {
-          final pokemonInfo = (state.fullInfo);
-          final urlImageString = pokemonInfo.pokemonApi.results[index].url;
-          final Uri url = Uri.parse(urlImageString);
-          final imageId = url.pathSegments[3];
-
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Flexible(
-                  child: Text(
-                    pokemonInfo.pokemonApi.results[index].name,
-                    style: PokeStyle.name,
-                  ),
-                ),
-                Image.network(
-                  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$imageId.png',
-                ),
-              ],
-            ),
-          );
-        } else {
-          return const CircularProgressIndicator(
-            color: PokeColor.darkRed,
-          );
-        }
-      },
     );
   }
 }
